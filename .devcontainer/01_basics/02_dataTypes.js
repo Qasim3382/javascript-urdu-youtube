6b1ae0b9879a38dab48a82bfb5b661c806b4ab4a
Syed Qasim Zafar <qasimzafar.work@gmail.com>
2025-08-20 08:48:07 +0000

    datatypes commit

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; //Treat all js code in newer version
+
+// number datatypes => 2 to power 53
+// big int
+// string datatypes = ""
+//boolean datatypes = true or false
+// null datatypes = standAlone value
+// undefined =>
+// symbols => uniqueness value kay liyay istamal hotay hain 
+
